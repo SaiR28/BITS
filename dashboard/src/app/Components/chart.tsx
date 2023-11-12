7569a5f93f5ec0a28bcfc8193672bd5f00b269a1
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-11-13 00:50:58 +0530

    stuff

--- dashboard/src/app/Components/chart.tsx
+++ dashboard/src/app/Components/chart.tsx
@@ -1,5 +1,5 @@
-
 import React from "react";
+import useSWR from 'swr'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,8 +11,6 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import { random } from "animejs";
-
 
 ChartJS.register(
   CategoryScale,
@@ -36,35 +34,52 @@ const options = {
   },
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
-const data = {
-  labels,
-  datasets: [
-    {
-      label: "Heart Rate",
-      data: labels.map(() => (Math.random()*2000)-1000),
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "SpO2",
-      data: labels.map(() => (Math.random()*2000)-1000),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-    {
-      label: "Hours Slept",
-      data: labels.map(() => (Math.random()*2000)-1000),
-      borderColor: "#FF9900",
-      backgroundColor: "#FF990044",
-    },
-  ],
+
+
+const fetcher:any = async (...args: Parameters<typeof fetch>): Promise<any> => {
+  const response = await fetch(...args);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
 };
 
-export function Chart(params:any) {
+export function Chart(params: any) {
+  const { data: APIData, error } = useSWR(
+    `http://127.0.0.1:5000/live_Data`,
+    fetcher,
+    { refreshInterval: 10 }
+  );
+
+
+  if (error)
+    return (
+      <div className="bg-[#22020266] w-64 h-40 rounded-lg p-2 ">
+        Failed to load
+      </div>
+    );
+  if (!APIData)
+    return <div className="w-64 h-40 bg-[#02221866] rounded-lg p-2"></div>;
+  // console.log(APIData['data'])
+  const labels=APIData['data']
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Heart Rate",
+        data: APIData['data'],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
   return (
-    <div className={"w-[36rem] border border-1 rounded-lg border-gray-100 p-2 mt-4 ml-4"}>
+    <div
+      className={
+        "w-[36rem] border border-1 rounded-lg border-gray-100 p-2 mt-4 ml-4"
+      }
+    >
       <h1 className="font-bold text-xl">{params.title}</h1>
       <Line options={options} data={data} />
     </div>
--- dashboard/src/app/Dashboards/Patient/Diagnosis/page.tsx
+++ dashboard/src/app/Dashboards/Patient/Diagnosis/page.tsx
@@ -1,47 +1,90 @@
-'use client'
+"use client";
+import useSWR from "swr";
+
 import { Chart } from "@/app/Components/chart";
 import { SvgSpinnersPulse } from "@/app/Components/icons";
 import { useState } from "react";
 
 export default function App() {
-const [health,setHealth]=useState(true)
+  const [health, setHealth] = useState(true);
 
   return (
     <div>
       <div className="fixed right-0 top-40">
-        <button onClick={()=>{
-            setHealth(!health)
-        }} className=" bg-red-300 h-12 rounded-l-2xl flex items-center p-3 hover:bg-red-500">
+        <button
+          onClick={() => {
+            setHealth(!health);
+          }}
+          className=" bg-red-300 h-12 rounded-l-2xl flex items-center p-3 hover:bg-red-500"
+        >
           Toggle Health
         </button>
       </div>
       {/* REMOVE THISS */}
 
       <Chart />
-     
-      <Prediction health={health}/>
+
+      <Prediction health={health} />
     </div>
   );
 }
 
+const fetcher: any = async (
+  ...args: Parameters<typeof fetch>
+): Promise<any> => {
+  const response = await fetch(...args);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
+};
+let i = 0;
+const pred_ = {
+  'N': "Normal beat",
+  'S': "Supraventricular premature beat",
+  'V': "Premature ventricular contraction",
+  'F': "Fusion of ventricular and normal beat",
+  'Q': "Unclassifiable beat",
+  'M': "myocardial infarction",
+};
+function Prediction(props: any) {
+  const { data: APIData, error } = useSWR(
+    `http://127.0.0.1:5000/Predict`,
+    fetcher,
+    { refreshInterval: 5000 }
+  );
 
-function Prediction(props:any){
-if(props.health){
-    return  <div className="m-3  border border-1 rounded-lg border-emerald-600 bg-emerald-500/20  p-3 flex w-96">
-    <h1 className="">AI Prediction : Healthy</h1>
-  </div>
-}
-return <div className="m-3  border border-1 rounded-lg border-red-600 bg-red-500/20  p-3 relative w-96 animate-pulse  ">
-<h1 className="">AI Prediction : Abnormal Heart Rate</h1>
-<div>
-  <div className="text-sm flex items-center gap-4">
-    Notifying Emergency Services
-    <SvgSpinnersPulse className="text-blue-600" />
-  </div>
+  if (error)
+    return (
+      <div className="bg-[#22020266] w-64 h-40 rounded-lg p-2 ">
+        Failed to load
+      </div>
+    );
+  if (!APIData)
+    return <div className="w-64 h-40 bg-[#02221866] rounded-lg p-2"></div>;
+  
+  console.log(APIData['pred']['label'])
 
-  <button className="mt-2 transition bg-red-500/50 hover:bg-red-500 p-1 px-2 rounded-lg border border-red-600">
-    Cancel
-  </button>
-</div>
-</div>
+  if (APIData['pred']['label']=='N') {
+    return (
+      <div className="m-3  border border-1 rounded-lg border-emerald-600 bg-emerald-500/20  p-3 flex w-96">
+        <h1 className="">AI Prediction : {String(pred_[APIData['pred']['label']])}</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="m-3  border border-1 rounded-lg border-red-600 bg-red-500/20  p-3 relative w-96 animate-pulse  ">
+      <h1 className="">AI Prediction : Abnormal Heart Rate : {String(pred_[APIData['pred']['label']])}</h1>
+      <div>
+        <div className="text-sm flex items-center gap-4">
+          Notifying Emergency Services
+          <SvgSpinnersPulse className="text-blue-600" />
+        </div>
+
+        <button className="mt-2 transition bg-red-500/50 hover:bg-red-500 p-1 px-2 rounded-lg border border-red-600">
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
 }
